3a33381fff28301fb050607ba1266458dfd192d5
Ashish Singh <ashishsingh.1899@gmail.com>
2023-06-02 22:04:17 +0530

    added register/login route

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 dotenv.config();
 
@@ -22,8 +24,18 @@ const flightSchema = new mongoose.Schema({
   flightName: String,
 });
 
+// Define the user schema
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+
 // Create a Flight model based on the schema
 const Flight = mongoose.model("Flight", flightSchema, "flightData");
+
+// Create a User model based on the schema
+const User = mongoose.model("User", userSchema, "users");
 
 // API endpoint for flight search
 app.get("/flights", async (req, res) => {
@@ -55,6 +67,61 @@ app.get("/flights", async (req, res) => {
     res.json(flightPrices);
   } catch (error) {
     console.error("Error searching flights:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+});
+
+// Register route
+app.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists." });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully." });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+});
+
+// Login route
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials." });
+    }
+
+    // Validate password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials." });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    res.json({ token });
+  } catch (error) {
+    console.error("Error logging in:", error);
     res.status(500).json({ message: "Internal server error." });
   }
 });
