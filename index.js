6bec76eeb713bac45fe8e257791219d79d38ec5d
Ashish Singh <ashishsingh.1899@gmail.com>
2023-06-02 21:18:31 +0530

    flight-api-with-mongodb-setup

--- index.js
+++ index.js
@@ -12,6 +12,52 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Define the flight schema
+const flightSchema = new mongoose.Schema({
+  source: String,
+  destination: String,
+  fare: String,
+  departureDate: String,
+  duration: String,
+  flightName: String,
+});
+
+// Create a Flight model based on the schema
+const Flight = mongoose.model("Flight", flightSchema, "flightData");
+
+// API endpoint for flight search
+app.get("/flights", async (req, res) => {
+  try {
+    const { source, destination } = req.query;
+
+    // Perform the search based on source and destination
+    const flights = await Flight.find({
+      source: { $regex: new RegExp(source, "i") },
+      destination: { $regex: new RegExp(destination, "i") },
+    });
+
+    if (flights.length === 0) {
+      return res.status(404).json({ message: "No flights found." });
+    }
+
+    // Display flight prices
+    const flightPrices = flights.map((flight) => {
+      console.log(flight);
+      return {
+        source: flight.source,
+        destination: flight.destination,
+        fare: flight.fare,
+        flightName: flight.flightName,
+      };
+    });
+
+    res.json(flightPrices);
+  } catch (error) {
+    console.error("Error searching flights:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
+});
+
 // Start the server
 const PORT = process.env.port || 4000;
 app.listen(PORT, () => {
